003e6a95b6dcc657ca26062f7c356e5cedf7ea74
Martins Veiskats <martins.veiskats@fjordsoftware.eu>
2016-11-22 23:50:47 +0200

    Implemented bulk database inserts

--- app/models/concerns/log_parser.rb
+++ app/models/concerns/log_parser.rb
@@ -1,10 +1,53 @@
 module LogParser
-  # Assigns named matches of MatchData to attributes of record
-  def self.merge_match_into_record(match_data, record)
-    return if record.nil?
+  # Do database writes in batches for speed
+  class WriteBuffer
+    FLUSH_THRESHOLD = 1000
 
-    match_data.names.each do |match_name|
-      record[match_name] = match_data[match_name]
+    def initialize
+      @bulk_entries = []
+    end
+
+    def <<(entry)
+      @bulk_entries << entry
+      flush if @bulk_entries.length >= FLUSH_THRESHOLD
+    end
+
+    def flush
+      LogEntry.import @bulk_entries
+      @bulk_entries = []
+    end
+  end
+
+  # Log lines need to be sorted into separate buffers by pid
+  # because lines of log messages from different processes can intermix
+  class Demultiplexer
+    def initialize
+      @pid_map = {}
+      @write_buffer = WriteBuffer.new
+    end
+
+    def [](pid)
+      @pid_map[pid]
+    end
+
+    def new_entry(pid, attributes)
+      @write_buffer << @pid_map[pid] if @pid_map[pid].present?
+      @pid_map[pid] = LogEntry.new attributes.merge(pid: pid)
+    end
+
+    # Assigns named matches of MatchData to attributes of record
+    def merge_match(pid, match_data)
+      entry = @pid_map[pid]
+      return if entry.nil?
+
+      match_data.names.each do |match_name|
+        entry[match_name] = match_data[match_name]
+      end
+    end
+
+    def flush
+      @pid_map.values.each { |entry| @write_buffer << entry }
+      @write_buffer.flush
     end
   end
 
@@ -15,6 +58,8 @@ module LogParser
 
     import_start = Time.now
 
+    buffer = WriteBuffer.new
+
     # TODO: Figure out a decent way to fit this into 80 char column width
     line_rx = /^(\w), \[(.+) #(\d+)\] .+ -- : (.*)$/
     started_rx = /^Started (?<method>\w+) "(?<uri>.+)" for (?<ip>.+) at/
@@ -23,9 +68,7 @@ module LogParser
     completed_rx = /^Completed (?<status_code>\d+) \w+ in (?<response_time>\d+)ms \(Views: (?<view_time>.+)ms \| ActiveRecord: (?<activerecord_time>.+)ms\)/
     user_rx = /^User id: (?<user_id>\d+)/
 
-    # Accumulate input through separate buffers for each pid,
-    # because log messages from different processes can intermix
-    buffer = {}
+    demux = Demultiplexer.new
 
     # Keep track of last known pid,
     # because some messages (exceptions) span multiple lines
@@ -38,13 +81,10 @@ module LogParser
       if match = line_rx.match(line)
         log_level, logged_at, pid, line_data = match.captures
         last_pid = pid
-        entry = buffer[pid]
 
         if match = started_rx.match(line_data)
-          buffer[pid].save if buffer[pid].present?
-          buffer[pid] = LogEntry.new logged_at: logged_at, pid: pid, content: ''
-          entry = buffer[pid]
-          merge_match_into_record(match, entry)
+          entry = demux.new_entry(pid, logged_at: logged_at, content: '')
+          demux.merge_match(pid, match)
 
           begin
             uri = URI.parse(entry.uri)
@@ -56,14 +96,16 @@ module LogParser
           end
 
         elsif match = processing_rx.match(line_data)
-          merge_match_into_record(match, entry)
+          demux.merge_match(pid, match)
         elsif match = parameters_rx.match(line_data)
-          merge_match_into_record(match, entry)
+          demux.merge_match(pid, match)
         elsif match = completed_rx.match(line_data)
-          merge_match_into_record(match, entry)
+          demux.merge_match(pid, match)
         elsif match = user_rx.match(line_data)
-          merge_match_into_record(match, entry)
+          demux.merge_match(pid, match)
         end
+
+        entry = demux[pid]
 
         if entry.present?
           entry.content += line
@@ -74,7 +116,7 @@ module LogParser
           end
         end
       else
-        entry = buffer[last_pid]
+        entry = demux[last_pid]
 
         if entry.present?
           entry.content += line
@@ -83,7 +125,7 @@ module LogParser
       end
     end
 
-    buffer.values.each &:save
+    demux.flush
 
     import_duration = sprintf('%.2f', Time.now - import_start)
     Rails.logger.info "Imported file #{file_name} in #{import_duration} seconds"
